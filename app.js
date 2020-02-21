7bb6a053141c1ddd16811743af4da241a2423f9b
AJIT-SINGH-RAWAT <47390463+AJIT-SINGH-RAWAT@users.noreply.github.com>
2020-02-21 17:06:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 
+//Listen for submit
+document.getElementById('loan-form').addEventListener('submit',function(e){
+   //Hide Results
+document.getElementById('loading').style.display='block';
+
+   //Show loader
+document.getElementById('results').style.display='none';
+setTimeout(calculateresult,2000);
+
+
+    e.preventDefault();
+});
